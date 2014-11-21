97970593335d4e90e9b5a4e751fd9cdd7555b0eb
ToddBailey <todd@narrat1ve.com>
2014-11-21 13:03:06 -0500

    Cleaned up sample packer, removed unused code and variables, added comments

--- tools/wtpaAudioSamplePacker/wtpaAudioSamplePacker.c
+++ tools/wtpaAudioSamplePacker/wtpaAudioSamplePacker.c
@@ -5,6 +5,7 @@
 
 // When pointed at a directory, takes all valid aiff files in that directory and makes them into a blob which can be put on a micro SD card
 // and played by a WTPA2.  File order is preserved (ie, alphabetical)
+// See "Usage" below for instructions
 
 // NOTES:
 // ----------
@@ -82,7 +83,7 @@ SSND chunk:
 #include <stdint.h>
 #include <string.h>			// For strcpy
 #include <sys/stat.h>		// For mkdir, fstat
-#include <time.h>			// For tagging destination directory with system time
+//#include <time.h>			// For tagging destination directory with system time
 #include <errno.h>			// For errno
 #include <dirent.h>			// For directory handling
 #include <limits.h>			// For PATH_MAX
@@ -97,32 +98,16 @@ typedef unsigned char bool;
 #define		false			(0)
 #define		true			(!(false))
 
-static char
-	*buffer;
-static uint32_t
-	targetFileLength;
 FILE
 	*targetFile;
 FILE
 	*outFile;
-
-// @@@ KILL ME
-static uint32_t
-	blobFileLength;
-//FILE
-//	*sourceFile;
-
 static unsigned int
 	sampleIndex;
 
 static char
 	commChunkId[4]={'C','O','M','M'},
 	soundChunkId[4]={'S','S','N','D'};
-
-static bool sampleInSlot[NUM_SAMPLES_MAX];		// Presence of sample in a given slot
-
-//static char* outputDirName;
-static char outputDirName[1024];
 
 static char *targetDirName;
 
@@ -141,233 +126,6 @@ static void Usage(void)
 	printf("Usage: wtpaAudioSamplePacker <target Directory>\n\n");
 	printf("Example: wtpaAudioSamplePacker ./aiffsToMakeABeatKnock\n\n");
 	printf("TMB, November 2014\n\n");
-}
-
-static bool GetHeaderData(void)
-// Get the WTPA ID and the number of samples in the file from the header.
-{
-	unsigned int
-		i,j,
-		sampleSlot,
-		numSamplesFound;
-
-	if((buffer[0]!='W')||(buffer[1]!='T')||(buffer[2]!='P')||(buffer[3]!='A'))		// See if this is any kind of WTPA2 data
-	{
-			printf("Bad Header! File does not start with \'WTPA\'.\n");
-			return(false);
-	}
-	if((buffer[4]!='S')||(buffer[5]!='A')||(buffer[6]!='M')||(buffer[7]!='P'))		// This is WTPA2 data, but not sample data?
-	{
-			printf("Non-sample data detected.  This is WTPA data but it does not appear to be standard audio samples.\n");
-			return(false);
-	}
-
-	sampleSlot=0;
-	numSamplesFound=0;
-
-	for(i=16;i<80;i++)		// Get TOC from bits set in this 64-byte long bitmask.
-	{
-		for(j=0;j<8;j++)
-		{
-			if(buffer[i]&(1<<j))
-			{
-				sampleInSlot[sampleSlot]=true;
-				numSamplesFound++;
-			}
-			else
-			{
-				sampleInSlot[sampleSlot]=false;
-			}
-
-			sampleSlot++;
-		}
-	}
-
-	if(numSamplesFound)
-	{
-		printf("Header good, found %d audio samples.\n",numSamplesFound);
-		return(true);
-	}
-
-	printf("Header looks OK but I can't see any samples in the TOC.\n");
-	return(false);
-}
-
-static bool MakeAiffDirectory(void)
-// Give the destination folder a unique name so we don't clobber folders accidentally.
-// NOTE:  Time code basically jacked from Wikipedia.  Thanks, Wikipedia.
-{
-	time_t
-		current_time;
-	char
-		tempString[256];
-
-	current_time=time(NULL);	// Obtain current time as seconds elapsed since the Epoch.
-
-	if(current_time==((time_t)-1))
-	{
-	    fprintf(stderr,"Failure to compute the current time.");
-	    return(false);
-	}
-
-//	outputDirName=ctime(&current_time);	// Convert to local time format.
-//
-//	if(outputDirName==NULL)
-//	{
-//	    fprintf(stderr,"Failure to convert the current time.");
-//	    return(false);
-//	}
-
-//	strtok(outputDirName,"\n");		// Remove trailing newline
-
-	strcpy(outputDirName,"wtpa2_samples_");
-
-	sprintf(tempString,"%ld",(unsigned long)current_time);
-	strcat(outputDirName,tempString);
-
-	if(mkdir(outputDirName,0777)==-1)
-	{
-	    fprintf(stderr,"Couldn't make destination folder.");
-	    return(false);
-	}
-
-	printf("Made AIFF directory: %s\n",outputDirName);
-	return(true);
-}
-
-static bool WriteSampleToAiff(unsigned int theSlot)
-// Make sure there are enough bytes in the buffer that this data can really be here.
-// Get the number of bytes in the file
-// Write the AIFF header
-// Write the audio data
-{
-	FILE
-		*destinationAiff;
-	unsigned int
-		i,
-		aiffBytes,
-		bytesInSoundChunk,
-		addressPointer,
-		numBytesInWtpaSample;
-
-	char
-		slotString[256],
-		filenameString[1024],
-		destString[1024];
-
-
-	if(((theSlot*1024*512)+512)>blobFileLength)		// Make sure we dont try to seek off the end of the buffer b/c of bad TOC
-	{
-	    fprintf(stderr,"Input blob too short to contain a sample at slot %d\n",theSlot);
-	    return(false);
-	}
-
-	addressPointer=((theSlot*1024*512)+512);	// Point at beginning of 512k sample slot (take into account the 512 byte header data)
-
-	// Sample length is first four bytes -- big endian
-
-	numBytesInWtpaSample=0;
-
-	numBytesInWtpaSample|=(buffer[addressPointer++]<<24)&0xFF000000;
-	numBytesInWtpaSample|=(buffer[addressPointer++]<<16)&0x00FF0000;
-	numBytesInWtpaSample|=(buffer[addressPointer++]<<8)&0x0000FF00;
-	numBytesInWtpaSample|=buffer[addressPointer++]&0xFF;
-
-	if(numBytesInWtpaSample>(512*1024))		// Sample cannot be this big.
-	{
-	    fprintf(stderr,"Bad sample in slot %u, sample size reads %u bytes\n",theSlot,numBytesInWtpaSample);
-	    return(false);
-	}
-
-	if(addressPointer+numBytesInWtpaSample>blobFileLength)	// Sample runs off file end?
-	{
-	    fprintf(stderr,"Sample in slot %u runs off end of input file!  Num bytes = %u\n",theSlot,numBytesInWtpaSample);
-	    return(false);
-	}
-
-	// Open aiff file
-
-	sprintf(slotString,"%d",theSlot);				// Number of sample slot to string
-	strcpy(filenameString,"wtpa2_sample_slot_");
-	strcat(filenameString,slotString);
-	strcat(filenameString,".aiff");
-
-	strcpy(destString,outputDirName);
-	strcat(destString,"/");
-	strcat(destString,filenameString);
-
-	destinationAiff=fopen(destString,"wb");	// Open the file for writing
-	if (!destinationAiff)
-	{
-		fprintf(stderr, "Can't create aiff file %s \n",destString);
-		return(false);
-	}
-
-	printf("Writing %d bytes from sample slot %d to AIFF output file...",numBytesInWtpaSample,theSlot);
-
-	// Stick AIFF chunks on to start
-
-	fprintf(destinationAiff,"FORM");											// IFF ID
-	aiffBytes = 4 + 8 + 18 + 16 + numBytesInWtpaSample;					// AIFF file byte total minus this chunk's ID and filesize = 46 non data bytes plus data: (4 left in ID chunk) + ((8+18) in comm chunk) + (16 in SSND chunk plus data and lead ins/outs)
-	fputc((aiffBytes & 0xff000000) >> 24,destinationAiff);
-	fputc((aiffBytes & 0x00ff0000) >> 16,destinationAiff);
-	fputc((aiffBytes & 0x0000ff00) >> 8,destinationAiff);
-	fputc((aiffBytes & 0x000000ff),destinationAiff);
-	fprintf(destinationAiff,"AIFF");
-
-	/* Write the common chunk */
-	fprintf(destinationAiff,"COMM");
-	fputc(0,destinationAiff);                               // Size of common chunk
-	fputc(0,destinationAiff);
-	fputc(0,destinationAiff);
-	fputc(18,destinationAiff);
-	fputc(0,destinationAiff);                               // One channel
-	fputc(1,destinationAiff);
-	fputc((numBytesInWtpaSample & 0xff000000) >> 24,destinationAiff);   // Sample frames are number of data bytes in mono 8-bit samples.
-	fputc((numBytesInWtpaSample & 0x00ff0000) >> 16,destinationAiff);
-	fputc((numBytesInWtpaSample & 0x0000ff00) >> 8,destinationAiff);
-	fputc((numBytesInWtpaSample & 0x000000ff),destinationAiff);
-	fputc(0,destinationAiff);                               // Sample size is 8 bits per sample point.
-	fputc(8,destinationAiff);
-	fputc(0x40,destinationAiff);							// 10 byte sample rate in IEEE extended
-	fputc(0x0d,destinationAiff);							// @@@ Fixed at 22050 for now.  Change this byte for 44100 or 11025
-	fputc(0xac,destinationAiff);
-	fputc(0x44,destinationAiff);
-	fputc(0,destinationAiff);
-	fputc(0,destinationAiff);
-	fputc(0,destinationAiff);
-	fputc(0,destinationAiff);
-	fputc(0,destinationAiff);
-	fputc(0,destinationAiff);
-
-	/* Write the sound data chunk */
-	bytesInSoundChunk=numBytesInWtpaSample+8;		// Extra 8 is for the unused offset and block bytes
-
-	fprintf(destinationAiff,"SSND");
-	fputc((bytesInSoundChunk & 0xff000000) >> 24,destinationAiff);		// Size of sound chunk
-	fputc((bytesInSoundChunk & 0x00ff0000) >> 16,destinationAiff);
-	fputc((bytesInSoundChunk & 0x0000ff00) >> 8,destinationAiff);
-	fputc((bytesInSoundChunk & 0x000000ff),destinationAiff);
-	fputc(0,destinationAiff);                               			// Offset (0 in our app and most apps)
-	fputc(0,destinationAiff);
-	fputc(0,destinationAiff);
-	fputc(0,destinationAiff);
-	fputc(0,destinationAiff);                                			// Block size (0 in our app and most apps)
-	fputc(0,destinationAiff);
-	fputc(0,destinationAiff);
-	fputc(0,destinationAiff);
-
-
-	for(i=0;i<numBytesInWtpaSample;i++)
-	{
-		fputc(buffer[addressPointer++],destinationAiff);
-	}
-
-	fclose(destinationAiff);
-
-	printf("Done!\n");
-
-	return(true);
 }
 
 static bool IsDirectory(char *path)
@@ -541,23 +299,6 @@ static bool CheckRecommendedSampleRate(void)
 	return(false);
 }
 
-/*
-static bool IsCorrectSize(void)
-// Returns false if the sample is too big to fit in a sample slot.
-// IE, is <512k plus AIFF headers
-{
-	fseek(targetFile, 0, SEEK_END);				// Get file length
-	targetFileLength=ftell(targetFile);			// Store this for later
-	fseek(targetFile, 0, SEEK_SET);
-
-	if(targetFileLength>((512+1)*1024))			// Bigger than 512k plus a bit for the AIFF headers?
-	{
-		return(false);
-	}
-	return(true);
-}
-*/
-
 static int GetSampleLength(void)
 // Returns number of sound data bytes in the AIFF (sample frames)
 // This is a four-byte big endian number in the COMM chunk
@@ -644,13 +385,6 @@ static bool IsLegit(void)
 		legit=false;
 		printf("Not an AIFF!\n");		
 	}
-/*
-	else if(!IsCorrectSize())
-	{
-		legit=false;
-		printf("AIFF too long to fit in WTPA sample slot!\n");		
-	}
-*/
 	else if(!IsBitDepthCorrect())
 	{
 		legit=false;	
@@ -664,7 +398,7 @@ static bool IsLegit(void)
 
 	if(legit)
 	{
-		wtpaSampleLength=GetSampleLength();		// Store this for later since we need it anyway.
+		wtpaSampleLength=GetSampleLength();
 
 		if((wtpaSampleLength<1)||(wtpaSampleLength>(512*1024)))
 		{
@@ -689,7 +423,7 @@ static bool IsLegit(void)
 static int GetSampleRate(void)
 // Gets the sample rate from the AIFF's COMM chunk.
 // Converts it from IEEE 10-byte extended floating point to WTPA format.
-// @@@ unimplemented
+// @@@ unimplemented, figure out IEEE conversions later (also, WTPA doesn't natively encode sampling rate yet anyway)
 {
 	return(1);
 }
@@ -781,23 +515,9 @@ static void WriteTocToOutputFile(void)
 int main(int argc, char *argv[])
 {
 	unsigned int
-		i,j;
-	struct
-		stat fileStat;	// Structure needed to get file info
-	int
-		fileDescriptor;
-	char
-		*strtolPtr;
-	long
-		optionalFileLength;
+		i;
 	char
 		pathBuf[PATH_MAX];
-
-
-    DIR* 
-    	directoryDescriptor;
-//    struct dirent*
-//    	directoryEntry;
 
 	struct
 		dirent **namelist;
@@ -881,10 +601,6 @@ int main(int argc, char *argv[])
 			UpdateToc();				// Log any data in info about this sample we'll need for the WTPA header, write this at the end
 		}
 
-
-
-		// @@@ get rid of all these unused variables
-
         fclose(targetFile);
 
 		if(sampleIndex>=NUM_SAMPLES_MAX)
@@ -898,129 +614,6 @@ int main(int argc, char *argv[])
 	free(namelist);
     fclose(outFile);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-
-	//Open file
-	sourceFile = fopen(argv[1], "rb");
-	if (!sourceFile)
-	{
-		fprintf(stderr, "Unable to open file: %s -- is it there and do you have permission?)\n",argv[1]);
-		return;
-	}
-
-	printf("\nReading WTPA2 sample data...\n");
-
-	fileDescriptor=fileno(sourceFile);	// Get file descriptor from file object
-
-	if(fstat(fileDescriptor,&fileStat)<0)	// Try to get file stats
-	{
-		fprintf(stderr, "Cannot get file stats for this file.");
-		return;
-	}
-
-	if(S_ISBLK(fileStat.st_mode))	// See if this is a block device (like an SD card, say)
-	{
-		if(argc==3)
-		{
-			optionalFileLength=strtol(argv[2],&strtolPtr,10);
-
-			if((optionalFileLength>256)||(optionalFileLength<0))	// Only 256MB of samples possible, and obviously negative is bad
-			{
-				blobFileLength=(256*1024*1024);
-				printf("Sorry, %ldMB doesn't make sense, reading 256MB.\n",optionalFileLength);
-			}
-			else
-			{
-				printf("\nReading %ldMB from block device...\n",optionalFileLength);
-				blobFileLength=(optionalFileLength*1024*1024);
-			}
-		}
-		else
-		{
-			printf("\nReading from block device, no size specified. Reading 256MB (this may a few minutes...)\n");
-			blobFileLength=(256*1024*1024);
-		}
-	}
-	else
-	{
-		printf("\nReading standard file.\n");
-
-		fseek(sourceFile, 0, SEEK_END);				//Get file length
-		blobFileLength=ftell(sourceFile);			// Store this for later
-		fseek(sourceFile, 0, SEEK_SET);
-
-		// Cap limit on read amount to what we can use
-		if(blobFileLength>(256*1024*1024))	// Only 256MB of samples possible
-		{
-			blobFileLength=(256*1024*1024);
-			printf("Only using useful sample area (first 256MB) of passed file.");
-		}
-	}
-
-	//Allocate memory
-	buffer=(char *)malloc(blobFileLength+1);
-	if (!buffer)
-	{
-		fprintf(stderr, "Error allocating memory for sample blob, exiting!\n");
-		fclose(sourceFile);
-		return;
-	}
-
-	//Read file contents into buffer
-	fread(buffer, blobFileLength, 1, sourceFile);
-	fclose(sourceFile);						// Close the file
-
-	// Report on the file length
-	printf("Blob file length in bytes = %u\n",blobFileLength);
-
-	// Check the header
-	if(GetHeaderData()==false)
-	{
-		fprintf(stderr, "Error in WTPA2 file header, exiting.\n");
-		return;
-	}
-
-	// Good header, have samples.  Read them in.
-
-	// Make directory for recovered AIFFs
-	if(MakeAiffDirectory()==false)
-	{
-		fprintf(stderr, "Couldn't make directory to store AIFFs, exiting.\n");
-		return;
-	}
-
-	// Go through all samples and put them in the directory
-	for(i=0;i<NUM_SAMPLES_MAX;i++)
-	{
-		if(sampleInSlot[i])		// NOTE -- you can "force" an erased sample to be read back by making this check true (erasing just clears a bit in the TOC, the sample is still there until overwritten)
-		{
-			if(WriteSampleToAiff(i)==false)
-			{
-				fprintf(stderr, "Error writing AIFF file %d, exiting.\n",i);
-				return;
-			}
-		}
-	}
-
-	// Done!
-	free(buffer);		// This will complain if we change the buffer's address, so we have to make an alias (see above)
-	printf("Sample files successfully converted.\n\n");
-*/
 	printf("Done!\n\n");
 	return(0);
 }
